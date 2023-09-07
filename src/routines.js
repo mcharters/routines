dc622318c68622299f44b945a0d349c2319ce0f9
Matthew Charters <matthew@charitycan.ca>
2023-09-06 21:48:15 -0400

    Much better (quicker) first version

---
+++ src/routines.js
@@ -0,0 +1,28 @@
+const routines = [
+  {
+    id: 1,
+    name: "Breakfast",
+    tasks: [
+      "Wake up",
+      "Brush teeth",
+      "Get dressed",
+      "Eat breakfast",
+      "Clear table",
+      "Pack backpack",
+      "Take medicine",
+      "Put on outdoor clothes",
+    ],
+  },
+  {
+    id: 2,
+    name: "After School",
+    tasks: [
+      "Take off outdoor clothes",
+      "Put away backpack",
+      "Wash hands",
+      "Eat snack",
+    ],
+  },
+];
+
+export default routines;
