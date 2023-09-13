e905c73ac7fe33676fd20f3512ddf513664bbb24
Matthew Charters <matthew@charitycan.ca>
2023-09-12 21:27:41 -0400

    Add some more routines

--- src/routines.js
+++ src/routines.js
@@ -6,6 +6,7 @@ const routines = [
       { name: "Wake up", complete: false },
       { name: "Brush teeth", complete: false },
       { name: "Get dressed", complete: false },
+      { name: "Make bed", complete: false },
       { name: "Eat breakfast", complete: false },
       { name: "Clear table", complete: false },
       { name: "Pack backpack", complete: false },
@@ -21,6 +22,28 @@ const routines = [
       { name: "Put away backpack", complete: false },
       { name: "Wash hands", complete: false },
       { name: "Eat snack", complete: false },
+    ],
+  },
+  {
+    id: 3,
+    name: "Dinner Time",
+    tasks: [
+      { name: "Wash hands", complete: false },
+      { name: "Eat dinner", complete: false },
+      { name: "Sit at table", complete: false },
+      { name: "Clear table", complete: false },
+    ],
+  },
+  {
+    id: 4,
+    name: "Bedtime",
+    tasks: [
+      { name: "Bath or shower", complete: false },
+      { name: "Put on pajamas", complete: false },
+      { name: "Snack", complete: false },
+      { name: "Snack tidied up", complete: false },
+      { name: "Brush teeth", complete: false },
+      { name: "YouTube time", complete: false },
     ],
   },
 ];
